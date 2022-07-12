e401fea61664409bf2814b97236f01a573b1fee4
Irina Khvatova <i.chwatowa@mail.ru>
2022-07-12 20:14:41 +0300

    Code changed

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,30 @@
 public class Main {
+
     public static void main(String[] args) {
 
         CreditPaymentService service = new CreditPaymentService();
 
-        int creditSum = 1_000_000;
-        double interestRate = 0.008325;
-        int creditPeriod = 12;
-        double monthlyAnnuityPayment = service.calculate(creditSum, interestRate, creditPeriod);
-        System.out.println("Сумма кредита (в руб.): " + creditSum);
+        double monthlyAnnuityPayment = service.calculate(1_000_000, 0.008325, 12);
+        System.out.println("Сумма кредита (в руб.): " + 1_000_000);
         System.out.println("Процентная ставка: 9,99%");
-        System.out.println("Срок кредита: " + creditPeriod + " месяцев");
+        System.out.println("Срок кредита: " + 12 + " месяцев");
         System.out.printf("Ежемесячный платеж по кредиту составит (в руб.): " + "%.0f", monthlyAnnuityPayment);
         System.out.println();
 
-        int creditSum1 = 1_000_000;
-        double interestRate1 = 0.008325;
-        int creditPeriod1 = 24;
-        double monthlyAnnuityPayment1 = service.calculate1(creditSum1, interestRate1, creditPeriod1);
+        double monthlyAnnuityPayment1 = service.calculate(1_000_000, 0.008325, 24);
         System.out.println();
-        System.out.println("Сумма кредита (в руб.): " + creditSum1);
+        System.out.println("Сумма кредита (в руб.): " + 1_000_000);
         System.out.println("Процентная ставка: 9,99%");
-        System.out.println("Срок кредита: " + creditPeriod1 + " месяца");
+        System.out.println("Срок кредита: " + 24 + " месяца");
         System.out.printf("Ежемесячный платеж по кредиту составит (в руб.): " + "%.0f", monthlyAnnuityPayment1);
         System.out.println();
 
-        int creditSum2 = 1_000_000;
-        double interestRate2 = 0.008325;
-        int creditPeriod2 = 36;
-        double monthlyAnnuityPayment2 = service.calculate2(creditSum2, interestRate2, creditPeriod2);
+        double monthlyAnnuityPayment2 = service.calculate(1_000_000, 0.008325, 36);
         System.out.println();
-        System.out.println("Сумма кредита (в руб.): " + creditSum2);
+        System.out.println("Сумма кредита (в руб.): " + 1_000_000);
         System.out.println("Процентная ставка: 9,99%");
-        System.out.println("Срок кредита: " + creditPeriod2 + " месяцев");
+        System.out.println("Срок кредита: " + 36 + " месяцев");
         System.out.printf("Ежемесячный платеж по кредиту составит (в руб.): " + "%.0f", monthlyAnnuityPayment2);
+        System.out.println();
     }
 }
